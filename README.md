a5a9818b3c7e1126c5716859c3ee74a09bb709c9
Cosmos <152656932+thomasthnucearbomb@users.noreply.github.com>
2024-03-06 01:16:09 +0000

    Pending changes exported from your codespace

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a repsoitory for random projects
---
+++ index.cs
@@ -0,0 +1,9 @@
+using System;
+namespace myFirstProgram{
+    class Index{
+        static void Main(string[] args){
+            Console.WriteLine("Hello");
+            Console.WriteLine("World!");
+        }
+    }
+}
